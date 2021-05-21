628c7343a2c236165c89b482690a559966701d84
Samantha Humphrey <samahump546@outlook.com>
2021-05-21 10:05:16 -0400

    method returns added

--- Conversion/Form1.cs
+++ Conversion/Form1.cs
@@ -24,47 +24,51 @@ namespace Conversion
             if (choiceInput.Text == "1")
             {
                 double inches = Convert.ToDouble(valueInput.Text);
-                InchesToCm(inches);
+                double answer = InchesToCm(inches);
+                convertOutput.Text = $"{inches} inches = {answer.ToString("0.00")} centimetres";
             }
             else if (choiceInput.Text == "2")
             {
                 double feet = Convert.ToDouble(valueInput.Text);
-                FeetToCm(feet);
+                double result = FeetToCm(feet);
+                convertOutput.Text = $"{feet} feet = {result.ToString("0.00")} centimetres";
             }
             else if (choiceInput.Text == "3")
             {
                 double yards = Convert.ToDouble(valueInput.Text);
-                YardsToMetres(yards);
+                double outcome = YardsToMetres(yards);
+                convertOutput.Text = $"{yards} yards = {outcome.ToString("0.00")} metres";
             }
             else if (choiceInput.Text == "4")
             {
                 double miles = Convert.ToDouble(valueInput.Text);
-                MilesToKm(miles);
+                double sum = MilesToKm(miles);
+                convertOutput.Text = $"{miles} miles = {sum.ToString("0.00")} kilometres";
             }
         }
 
-        public void InchesToCm(double inches)
+        public double InchesToCm(double inches)
         {
             double cm = inches * 2.54;
-            convertOutput.Text = $"{inches} inches = {cm.ToString("0.00")} centimetres";
+            return cm;
         }
 
-        public void FeetToCm(double feet)
+        public double FeetToCm(double feet)
         {
             double cm = feet * 30.48;
-            convertOutput.Text = $"{feet} feet = {cm.ToString("0.00")} centimetres"; 
+            return cm;
         }
 
-        public void YardsToMetres(double yards)
+        public double YardsToMetres(double yards)
         {
             double metres = yards * 0.91;
-            convertOutput.Text = $"{yards} yards = {metres.ToString("0.00")} metres";
+            return metres;
         }
 
-        public void MilesToKm(double miles)
+        public double MilesToKm(double miles)
         {
             double km = miles * 1.6;
-            convertOutput.Text = $"{miles} miles = {km.ToString("0.00")} kilometres";
+            return km;
         }
     }
 
